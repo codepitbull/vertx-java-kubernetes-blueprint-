8a65e0a6772813f1e2cde3ef59b7c63f6ec23d83
Jochen Mader <pflanzenmoerder@gmail.com>
2018-06-14 10:30:19 +0200

    More thorough example of eventbus

--- src/main/java/de/codepitbull/vertx/kubernetes/blueprint/MainVerticle.java
+++ src/main/java/de/codepitbull/vertx/kubernetes/blueprint/MainVerticle.java
@@ -5,6 +5,21 @@ import io.vertx.reactivex.core.AbstractVerticle;
 public class MainVerticle extends AbstractVerticle {
     @Override
     public void start() throws Exception {
-        vertx.createHttpServer().requestHandler(r -> r.response().end("I AM UP")).listen(8666);
+        vertx.eventBus().<Long>consumer("ping", msg -> {
+            System.out.println("I AM (" + hashCode() + ") and talked to(" + msg.body() + ")");
+            msg.reply(hashCode());
+        });
+        vertx
+                .createHttpServer()
+                .requestHandler(r -> {
+                    vertx.eventBus().<Long>send("ping", hashCode(), response -> {
+                        if (response.succeeded()) {
+                            r.response().end("I AM (" + hashCode() + ") and talked to(" + response.result().body() + ")");
+                        } else {
+                            r.response().end("I AM (" + hashCode() + ")");
+                        }
+                    });
+                })
+                .listen(8666);
     }
 }
